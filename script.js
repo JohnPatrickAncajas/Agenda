21283012ce92dee54fb958b501be869fdc8dad8d
JohnPatrickAncajas <ancajasjohnpatrickdelapena@gmail.com>
2023-10-21 22:52:24 +0800

    Version 3
    Change the map to arrays for better control.
    Divided the main function to two for better control.
    Changed the layout and design of task elements.
    Moved HTML parts to JS.
    Removed unecessary code.

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-const agenda = new Map();
-agenda.set("Hello!", "Have a Good Day!");
+let agenda = [
+    {time: "6:00AM", task: "Good Morning! It's a brand new day, take care!"},
+];
 
 function addTask() {
+
+    const taskList = document.querySelector("#taskList");
+    taskList.innerHTML = "";
+
     const taskInput = document.getElementById("taskInput");
     const timeInput = document.getElementById("taskTime");
 
@@ -9,32 +14,59 @@ function addTask() {
     const taskTime = timeInput.value.trim();
     
     if (taskText !== "" && taskTime !== "") {
-        
-        if (agenda.has(taskTime)) {
-            const taskTextNew = agenda.get(taskTime) + taskText;
-            agenda.set(taskTime, taskTextNew);
+
+        const hours = taskTime[0] + taskTime[1];
+        const minutes = taskTime[3] + taskTime[4];
+
+        const hoursValue = parseInt(hours, 10);
+        const minutesValue = parseInt(minutes, 10);
+
+        if (minutesValue < 10) {
+            minutesDisplayValue = "0" + minutesValue;
         } else {
-            agenda.set(taskTime, taskText);
-        }
-        
-    }
-
-    for (const value of agenda) {
-        console.log(value[1]);
-
-        var taskList = document.getElementById("taskList");
-
-        while (taskList.firstChild) {
-            taskList.removeChild(taskList.firstChild);
+            minutesDisplayValue = minutesValue;
         }
 
-        let newTaskDiv = document.createElement("div");
-        newTaskDiv.className = "task";
-        newTaskDiv.idName = "task#" + value;
-        newTaskDiv.innerText = value[0] + " " + value[1];
-        document.getElementById("taskList").appendChild(newTaskDiv);
-    }     
+        const dayPeriod = hoursValue < 12 ? "AM" : "PM";
+        const hoursDisplayValue = (hoursValue % 12) || 12;
+
+        const newTask = {time: hoursDisplayValue + ":" + minutesDisplayValue + dayPeriod, task: taskText};
+        agenda.push(newTask);
+
+        addTaskElement();
 
     taskInput.value = "";
     timeInput.value = "";
+    }
 }
+
+// During the creation of this part is the first time JavaScript hit me with [object Object][object Object]!
+
+function addTaskElement() {
+    for (let i = 0; i < agenda.length; i++) {
+
+        const divElement = document.createElement("p");
+        const timeElement = document.createElement("p");
+        const taskElement = document.createElement("p");
+
+        divElement.className = "task";
+        timeElement.className = "timeClass";
+        timeElement.innerHTML = agenda[i].time + "<hr>";
+        taskElement.className = "taskClass";
+        taskElement.innerHTML = agenda[i].task;
+
+        divElement.appendChild(timeElement);
+        divElement.appendChild(taskElement)
+        taskList.appendChild(divElement);
+    }
+}
+
+window.addEventListener('load', function() {
+    const title = document.querySelector("#title");
+    const titleText = document.querySelector("#titleText");
+
+    title.textContent = "Agenda";
+    titleText.textContent = "Take control of your time and easily structure your day in just a few clicks!";
+
+    addTaskElement();
+});
