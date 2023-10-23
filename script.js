649ff3f60b9ab835af90120543433a438137833e
JohnPatrickAncajas <ancajasjohnpatrickdelapena@gmail.com>
2023-10-23 18:58:51 +0800

    Fixed array splice bug. Closes #1
    Tried fixing the bug for the whole day.
    Introduced the remove div bug.
    Rewritten the JavaScript code thrice to find a solution.
    Fixed the remove div bug.
    Fixed the array splice bug.

--- script.js
+++ script.js
@@ -1,100 +1,67 @@
 let agenda = [
-    {time: "6:00 AM", task: "Good Morning! It's a brand new day, take care!"},
+    { time: "6:00 AM", task: "Good Morning! It's a brand new day, take care!" },
 ];
 
 function addTask() {
-
-    const taskList = document.querySelector("#taskList");
     const taskInput = document.getElementById("taskInput");
     const timeInput = document.getElementById("taskTime");
-
     const taskText = taskInput.value.trim();
     const taskTime = timeInput.value.trim();
-    
+
     if (taskText !== "" && taskTime !== "") {
-
-        taskList.innerHTML = "";
-
-        const hours = taskTime[0] + taskTime[1];
-        const minutes = taskTime[3] + taskTime[4];
-
+        const hours = taskTime.substring(0, 2);
+        const minutes = taskTime.substring(3, 5);
         const hoursValue = parseInt(hours, 10);
         const minutesValue = parseInt(minutes, 10);
-
-        if (minutesValue < 10) {
-            minutesDisplayValue = "0" + minutesValue;
-        } else {
-            minutesDisplayValue = minutesValue;
-        }
-
-        const dayPeriod = hoursValue < 12 ? " AM" : " PM";
+        const minutesDisplayValue = (minutesValue < 10) ? "0" + minutesValue : minutesValue;
+        const dayPeriodDisplay = hoursValue < 12 ? " AM" : " PM";
         const hoursDisplayValue = (hoursValue % 12) || 12;
 
-        const newTask = {time: hoursDisplayValue + ":" + minutesDisplayValue + dayPeriod, task: taskText};
+        const newTask = { time: hoursDisplayValue + ":" + minutesDisplayValue + dayPeriodDisplay, task: taskText };
         agenda.push(newTask);
 
-        updateTask();
+        taskInput.value = "";
+        timeInput.value = "";
 
-    taskInput.value = "";
-    timeInput.value = "";
+        updateTask();
     }
 }
 
-// During the creation of this part is the first time JavaScript hit me with [object Object][object Object]!
-
 function updateTask() {
+    const taskList = document.querySelector("#taskList");
+    taskList.innerHTML = "";
+
     for (let i = 0; i < agenda.length; i++) {
-
-        const divElement = document.createElement("p");
-        const timeElement = document.createElement("p");
-        const xElement = document.createElement("p");
-        const taskElement = document.createElement("p");
-
+        const divElement = document.createElement("div");
         divElement.className = "task";
-        divElement.id = "task" + i;
+
+        const timeElement = document.createElement("p");
         timeElement.className = "timeClass";
         timeElement.innerHTML = agenda[i].time;
+
+        const xElement = document.createElement("p");
         xElement.className = "xClass";
-        xElement.id = "x" + i;
         xElement.innerHTML = "&times;";
+        xElement.addEventListener("click", function () {
+            agenda.splice(i, 1);
+            updateTask();
+        });
+
+        const taskElement = document.createElement("p");
         taskElement.className = "taskClass";
         taskElement.innerHTML = "<hr>" + agenda[i].task;
 
-        const firstChild = taskList.firstElementChild;
-
         divElement.appendChild(timeElement);
         divElement.appendChild(xElement);
-        divElement.appendChild(taskElement)
-
-        taskList.insertBefore(divElement, firstChild);
-
-        const xButton = document.querySelectorAll(".xClass");
-
-        xButton.forEach(function(element) {
-            element.addEventListener("click", function() {
-
-            const charactersWhole = this.id;
-            const characters = charactersWhole.split("");
-            
-            const divToRemove = document.getElementById("task" + characters[1]);
-
-            const removeButton = document.getElementById("removeButton");
-
-            divToRemove.remove();
-            agenda.splice(characters[1]);
-            console.log(agenda);
-            
-            });
-        });
+        divElement.appendChild(taskElement);
+        taskList.appendChild(divElement);
     }
 }
-  
-window.addEventListener("load", function() {
+
+window.addEventListener("load", function () {
     const title = document.querySelector("#title");
     const titleText = document.querySelector("#titleText");
-
     title.textContent = "Agenda";
     titleText.textContent = "Take control of your time and easily structure your day in just a few clicks!";
-
     updateTask();
 });
